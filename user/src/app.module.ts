c87c1761c3e7eef3fadae6522a366c30e6ade594
ibadin <ibmeshach@gmail.com>
2024-08-16 14:02:48 +0100

    changes

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -18,16 +18,12 @@ import { SmsModule } from './sms/sms.module';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.string().required(),
-        USER_X_API_KEY: Joi.string().required(),
-        JWT_ACCESS_TOKEN: Joi.string().required(),
         POSTGRES_HOST: Joi.string().required(),
         POSTGRES_PORT: Joi.number().required(),
         POSTGRES_USER: Joi.string().required(),
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DATABASE: Joi.string().required(),
-        TWILIO_ACCOUNT_SID: Joi.string().required(),
-        TWILIO_AUTH_TOKEN: Joi.string().required(),
+        JWT_ACCESS_TOKEN: Joi.string().required(),
         OTP_JWT_TOKEN: Joi.string().required(),
       }),
     }),
