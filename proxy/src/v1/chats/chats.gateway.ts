67a996c1424721742578faf370a7130e2c0abf8a
ibadin <ibmeshach@gmail.com>
2024-08-23 11:04:03 +0100

    changrs

--- proxy/src/v1/chats/chats.gateway.ts
+++ proxy/src/v1/chats/chats.gateway.ts
@@ -28,7 +28,11 @@ export class ChatGateway implements OnGatewayConnection {
   ) {}
 
   handleConnection(client: Socket) {
-    const { token, role, roomId } = client.handshake.query;
+    const token = client.handshake.headers['authorization'];
+
+    console.log(token);
+    const { role, roomId } = client.handshake.query;
+
     client.data = { token, role };
     client.join(roomId);
   }
@@ -42,6 +46,8 @@ export class ChatGateway implements OnGatewayConnection {
     const { token, role } = client.data;
 
     console.log('get here first', payload);
+
+    // Ensure that the observables are subscribed to
     this.driversClient
       .send(
         'driver.saveChatMessage',
@@ -54,9 +60,14 @@ export class ChatGateway implements OnGatewayConnection {
       )
       .pipe(
         catchError((error) => {
+          console.error('Error sending message to drivers service:', error);
           throw error;
         }),
-      );
+      )
+      .subscribe({
+        next: (response) => console.log('Driver service response:', response),
+        error: (error) => console.error('Driver service error:', error),
+      });
 
     this.usersClient
       .send(
@@ -70,9 +81,14 @@ export class ChatGateway implements OnGatewayConnection {
       )
       .pipe(
         catchError((error) => {
+          console.error('Error sending message to users service:', error);
           throw error;
         }),
-      );
+      )
+      .subscribe({
+        next: (response) => console.log('User service response:', response),
+        error: (error) => console.error('User service error:', error),
+      });
 
     console.log('get here second and last');
     this.server.to(roomId).emit('message', { message });
